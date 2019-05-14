eca09c707c4b9abefee74deabfb92543b80b0432
Yijia Chen <jyc.chen@mail.utoronto.ca>
2019-05-14 15:58:07 -0400

    Add connect graph option to gen

--- graph_gen.cpp
+++ graph_gen.cpp
@@ -1,10 +1,14 @@
 #include <cstdio>
 #include <errno.h>
+#include <queue>
 #include <random>
 #include <string>
 #include <unordered_set>
 #include <vector>
 #include "getopt.h"
+
+#define W_LB 0.0
+#define W_UB 1.0
 
 using namespace std;
 
@@ -28,16 +32,41 @@ double randdouble(double lb, double ub) {
 	return dist(rng);
 }
 
+void dfs(vector<unordered_set<int>> graph, vector<int> *colour, int x, int c) {
+	(*colour)[x] = c;
+	for (auto it = graph[x].begin(); it != graph[x].end(); ++it) {
+		if ((*colour)[*it] == -1) {
+			dfs(graph, colour, *it, c);
+		}
+	}
+}
+
+void bfs(vector<unordered_set<int>> graph, vector<int> *colour, int x, int c) {
+	queue<int> q;
+	q.push(x);
+
+	while (!q.empty()) {
+		x = q.front(), q.pop();
+		(*colour)[x] = c;
+
+		for (auto it = graph[x].begin(); it != graph[x].end(); ++it) {
+			if ((*colour)[*it] == -1) {
+				q.push(*it);
+			}
+		}
+	}
+}
+
 int main(int argc, char *argv[]) {
-	int N, M;
+	int N, M, d, c;
 	string output_file;
-	if (argc < 4) {
-		printf("Usage: %s -v <vertices> -e <edges> -o <filename>\n", argv[0]);
+	if (argc < 6) {
+		printf("Usage: %s -v <vertices> -e <edges> -d <directed> -c <connected> -o <filename>\n", argv[0]);
 		exit(1);
 	}
 
 	char option;
-	while ((option = getopt(argc, argv, "v:e:o:")) != -1) {
+	while ((option = getopt(argc, argv, "v:e:d:c:o:")) != -1) {
 		switch (option) {
 		case 'v':
 			N = atoi(optarg);
@@ -45,23 +74,43 @@ int main(int argc, char *argv[]) {
 		case 'e':
 			M = atoi(optarg);
 			break;
+		case 'd':
+			d = atoi(optarg);
+			if (d != 0 && d != 1) {
+				printf("Error: d must be either 0 or 1\n");
+				exit(1);
+			}
+			break;
+		case 'c':
+			c = atoi(optarg);
+			if (c != 0 && c != 1) {
+				printf("Error: c must be either 0 or 1\n");
+				exit(1);
+			}
+			if (c == 1 && d == 1) {
+				// Since we cannot guarantee a forest of DAGs are produced if d = 1
+				printf("Error: the graph may only be connected if undirected\n");
+				exit(1);
+			}
+			break;
 		case 'o':
 			output_file = string(optarg);
 			break;
 		default:
-			printf("Usage: %s -v <vertices> -e <edges> -o <filename>\n", argv[0]);
+			printf("Usage: %s -v <vertices> -e <edges> -d <directed> -c <connected> -o <filename>\n", argv[0]);
 			exit(1);
 		}
 	}
 
 	// Generate the graph
+	printf("Generating graph...\n");
 	vector<unordered_set<int>> graph(N);
 	unordered_set<pair<int, pair<int, double>>, pipid_hash> edges; // Random ordering
 
 	for (int i = 0; i < M; i++) {
 		int u = randint(0, N - 1);
 		int v = randint(0, N - 1);
-		double w = randdouble(0.0, 1.0);
+		double w = randdouble(W_LB, W_UB);
 		
 		if (u == v || graph[u].find(v) != graph[u].end()) {
 			i--;
@@ -69,10 +118,41 @@ int main(int argc, char *argv[]) {
 		}
 
 		graph[u].insert(v);
+		if (!d) graph[v].insert(u);
+		// Don't need to insert both edges (if undirected) since that's up to the user's algorithm
 		edges.insert({ u, {v, w} });
 	}
 
+	// If the undirected graph needs to be connected
+	if (!d && c) {
+		printf("Connecting graph...\n");
+		vector<int> colour(N, -1);
+		int col = 0;
+
+		for (int i = 0; i < N; i++) {
+			if (colour[i] == -1) {
+				//dfs(graph, &colour, i, col++);
+				bfs(graph, &colour, i, col++);
+			}
+		}
+
+		// Connect all forests to vertex 0, for simplicity
+		unordered_set<int> connected;
+		for (int i = 1; i < N; i++) {
+			if (colour[i] != colour[0] && connected.find(colour[i]) == connected.end()) {
+				edges.insert({ 0, {i, randdouble(W_LB, W_UB)} });
+				connected.insert(colour[i]);
+			}
+		}
+
+		if (col != 1) {
+			M += col - 1;
+			printf("Note: added %d edges to connect undirected graph\n", col - 1);
+		}
+	}
+
 	// Print the graph to the file
+	printf("Printing graph...\n");
 	FILE *fd = fopen(output_file.c_str(), "w");
 	if (!fd) {
 		perror("fopen");
